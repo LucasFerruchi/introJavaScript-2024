5394f24530d12cb4adf6f91cc3e3690826ecb4fb
LucasFerruchi <lucasferruchi05@gmail.com>
2024-09-30 21:29:47 -0300

    soluciones a las tareas de 05_tareasFor.js

--- js/05_tareasFor.js
+++ js/05_tareasFor.js
@@ -1,6 +1,15 @@
 //? FOR
 
-//Ejercicios TABLAS: pedir al usuario la tabla a calcular
+//Ejercicios TABLAS: pedir al usuario la tabla a calcular:
+
+// let tablaFor = parseInt(prompt("Ingrese la tabla a calcular!"));
+
+// console.log(`Esta es la tabla del ${tablaFor} 😁`);
+
+// //Iniciamos la variable el 1, para que no arranque en 0 la tabla
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${tablaFor} x ${i} = ${tablaFor * i}`);
+// }
 
 //* ----------------------------------------------------
 //? DO WHILE y FOR
@@ -8,6 +17,21 @@
 //Ejercicio MULTITABLAS:
 /*Permitir al usuario pedir cualquier tabla */
 // let multiTabla = null;
+
+// do {
+//   multiTabla = parseInt(prompt("Ingrese la tabla a calcular"));
+//   if (multiTabla) {
+//     console.log("==================");
+//     console.log(`Tabla del ${multiTabla} 🤩`);
+//     console.log("==================");
+
+//     for (let i = 1; i <= 10; i++) {
+//       console.log(`${multiTabla} x ${i} = ${multiTabla * i}`);
+//     }
+//   } else {
+//     console.log("No es una tabla válida! 😪");
+//   }
+// } while (multiTabla);
 
 //* -----------------------------------------------------
 //TAREA: Lista de alumnos:
@@ -20,3 +44,28 @@ arroje la lista en consola
 - Generar una lista
 - Mostrar la lista
 */
+
+// let cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos!"));
+// console.log(cantidadAlumnos);
+// let nombreAlumno = "";
+// let lista = "";
+// //Primero evaluar si el dato ingresado es un numero
+
+// if (cantidadAlumnos) {
+//   for (let i = 0; i < cantidadAlumnos; i++) {
+//     nombreAlumno = prompt("Ingrese el nombre");
+//     /*a continuacion un if con una condicion
+//     q valida que se halla completado el campo, o sea un true*/
+//     if (nombreAlumno) {
+//       lista += `${nombreAlumno}\n`; /* "\n" hace un salto de linea
+//       como el br en html, en este caso lo ponemos para que los nombres
+//       se vayan acomodando uno debajo del otro!!*/
+//     } else {
+//       //En el caso que el usuario no complete el campo o presione cancelar
+//       console.warn("Los datos no son validos!");
+//     }
+//   }
+//   console.log(lista);
+// } else {
+//   console.log("La cantidad ingresada no es valida!");
+//}
