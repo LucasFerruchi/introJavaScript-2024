ca3d2511e3241ec33f2f1b1d1a533261f802dea2
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2024-10-09 20:38:01 -0300

    clase de callbacks y metodos de arrays

---
+++ js/08-tarea.js
@@ -0,0 +1,10 @@
+// Ejercicio 1: Crear una función con un callback personalizado
+// Deben crear una función llamada procesarDatos que reciba un array y un callback. El callback determinará cómo procesar los datos del array. Puede ser una suma, resta, multiplicacion o division como el ejemplo de clases, pero usando map o foreach
+
+
+
+// Ejercicio 2: Filtrar y encontrar nombres con callbacks
+// Deben filtrar un array de nombres para encontrar aquellos que comiencen con una letra específica. Luego, deben encontrar el primero que cumpla esa condición.
+
+
+
